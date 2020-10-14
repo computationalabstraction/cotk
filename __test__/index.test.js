0a6c836b69e4b64e420821d6ec1d0e51dac75fef
Archan Patkar <pat@archanpatkar.com>
2020-10-14 20:41:55 +0530

    wip

---
+++ __test__/index.test.js
@@ -0,0 +1,60 @@
+console.log(takeRight([1, 2, 3]));
+
+console.log(takeRight([1, 2, 3], 2));
+// => [2, 3]
+ 
+console.log(takeRight([1, 2, 3], 5));
+// => [1, 2, 3]
+
+console.log(takeRight([1, 2, 3], 0));
+// =>  []
+
+// console.log(take([1, 2, 3]));
+// console.log(take([1, 2, 3], 2));
+// console.log(take([1, 2, 3], 5));
+// console.log(take([1, 2, 3], 0));
+
+// var array = [1, 2, 3];
+
+// console.log("fill");
+// fill(array, 'a');
+// console.log(array);
+// // => ['a', 'a', 'a']
+
+// console.log(fill(Array(3), 2));
+ 
+// console.log(fill([4, 6, 8, 10], '*', 1, 3));
+// // => [4, '*', '*', 10]
+
+
+// let a = [10,20,30,40,50,60,70,80,90];
+
+// console.log(chunk(a,3));
+
+// console.log(compact([0, 1, false, 2, '', 3]));
+
+
+// console.log(drop([1, 2, 3]));
+// // => [2, 3]
+ 
+// console.log(drop([1, 2, 3], 2));
+// // => [3]
+ 
+// console.log(drop([1, 2, 3], 5));
+// // => []
+ 
+// console.log(drop([1, 2, 3], 0));
+// // => [1, 2, 3]
+
+// console.log("dropRight");
+// console.log(dropRight([1, 2, 3]));
+// // => [1, 2]
+ 
+// console.log(dropRight([1, 2, 3], 2));
+// // => [1]
+ 
+// console.log(dropRight([1, 2, 3], 5));
+// // => []
+ 
+// console.log(dropRight([1, 2, 3], 0));
+// // => [1, 2, 3]
